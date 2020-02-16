67e8ee713d8d93ca8e0a3306f0b10fec7d21698e
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-02-16 16:30:45 -0700

    added function to display all items in store

--- jamazonCustomer.js
+++ jamazonCustomer.js
@@ -1,13 +1,28 @@
-const mysql = require("mysql"), inquirer = require("inquirer");
+const mysql = require("mysql"),
+  inquirer = require("inquirer");
 const connection = mysql.createConnection({
-    host: "locahost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "jamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "jamazon"
 });
 
-connection.connect(function (err) { 
-    if (err) throw err;
+connection.connect(function(err) {
+  if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
- })
+    displayItems()
+});
+
+function displayItems() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      console.log(
+        res[i].id + " | " + res[i].product_name + " | " + res[i].price
+      );
+    }
+    console.log("----------------------------------");
+    connection.end();
+  });
+}
