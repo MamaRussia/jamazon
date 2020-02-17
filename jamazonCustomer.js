e9e834241dd4c7e15c1054c1439fb83e774c39d8
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-02-16 17:16:21 -0700

    added welcome prompt

--- jamazonCustomer.js
+++ jamazonCustomer.js
@@ -1,28 +1,46 @@
 const mysql = require("mysql"),
-  inquirer = require("inquirer");
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "jamazon"
-});
+  inquirer = require("inquirer"),
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "jamazon"
+  });
 
 connection.connect(function(err) {
   if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    displayItems()
+  console.log("connected as id " + connection.threadId + "\n");
+  displayItems();
 });
 
 function displayItems() {
   connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
+      if (err) throw err;
+      console.log("Welcome to Jamazon. Check Out Our Products \n" + "---------------------------------");
+      
     for (let i = 0; i < res.length; i++) {
       console.log(
         res[i].item_id + " | " + res[i].product_name + " | " + res[i].price
       );
     }
     console.log("----------------------------------");
-    connection.end();
+    // connection.end();
+      
+      welcome()
   });
+}
+
+function welcome() {
+    inquirer.prompt({
+        name: "firstName",
+        type: "input",
+        message: "What is your first name?",
+        default: "Jamazon Guest"
+    }).then(function (answer) {
+        console.log("Hello " + answer.firstName);
+        
+})
+
+
 }
