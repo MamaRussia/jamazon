31511dfdb3ad069883cf8e23a4cebc32b220eeac
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-02-16 17:32:55 -0700

    added product search. prompt works but when searching db undefined thrown. need to fix

--- jamazonCustomer.js
+++ jamazonCustomer.js
@@ -16,9 +16,12 @@ connection.connect(function(err) {
 
 function displayItems() {
   connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log("Welcome to Jamazon. Check Out Our Products \n" + "---------------------------------");
-      
+    if (err) throw err;
+    console.log(
+      "Welcome to Jamazon. Check Out Our Products \n" +
+        "---------------------------------"
+    );
+
     for (let i = 0; i < res.length; i++) {
       console.log(
         res[i].item_id + " | " + res[i].product_name + " | " + res[i].price
@@ -26,21 +29,38 @@ function displayItems() {
     }
     console.log("----------------------------------");
     // connection.end();
-      
-      welcome()
+
+    welcome();
   });
 }
 
 function welcome() {
-    inquirer.prompt({
-        name: "firstName",
-        type: "input",
-        message: "What is your first name?",
-        default: "Jamazon Guest"
-    }).then(function (answer) {
+  inquirer
+    .prompt({
+      name: "firstName",
+      type: "input",
+      message: "What is your first name?",
+      default: "Jamazon Guest"
+    })
+    .then(function(answer) {
         console.log("Hello " + answer.firstName);
-        
-})
+        productSearch()
+    });
+}
 
-
+function productSearch() {
+    inquirer.prompt({
+        name: "product",
+        type: "input",
+        message: "Enter the item id of the product you want to buy."
+    }).then(function (answer) {
+        let query = "SELECT item_id FROM products WHERE ?";
+        connection.query(query, { product: answer.product }, function (err, res) {
+            for (let i = 0; i < res.length; i++) {
+                console.log("Item ID: " + res[i].item_id + " || Name: " + res[i].product_name);
+                
+                
+            }
+        })
+    })
 }
